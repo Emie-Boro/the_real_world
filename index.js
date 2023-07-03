4c6057e430281474e99f6da02300fac3952b56a5
Emie Boro <emiedonmukumo@gmail.com>
2023-07-03 16:47:30 +0100

    Update Url

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.post('/', async (req,res)=>{
     res.redirect('/')
 })
 
-app.get('/formdata/realworld/json', async (req,res) =>{
+app.get(formUrl, async (req,res) =>{
     const formdata = await Formdata.find()
 
     res.json(formdata)
